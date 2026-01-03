ab8bf42c09c2122b778e08ce7753867514254377
Phạm Trung Hiếu <161118087+hieup2003191111@users.noreply.github.com>
2026-01-03 17:06:29 +0700

    Delete .hieu_driver.mod.cmd

--- .hieu_driver.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/hieu/hoclinux/led/hieu_driver.mod := printf '%s\n'   hieu_driver.o | awk '!x[$$0]++ { print("/home/hieu/hoclinux/led/"$$0) }' > /home/hieu/hoclinux/led/hieu_driver.mod
